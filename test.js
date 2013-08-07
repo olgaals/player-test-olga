ae12fc34620074ef0fc439ce5d85ba8ba9fcd60d
Olga <osidorova@gmail.com>
2013-08-07 15:37:32 +0300

    Update test.js

--- test.js
+++ test.js
@@ -1,2 +1,21 @@
-player-test-olga
-================
+(function() {
+    console.log("****test share******************");
+    times=0;
+    deleted=0;
+
+ onPlayerReady=function(){
+    
+     console.log("ready");
+    
+}
+
+player = brightcove.api.getExperience();
+    videoPl = player.getModule(brightcove.api.modules.APIModules.VIDEO_PLAYER);
+    exper = player.getModule(brightcove.api.modules.APIModules.EXPERIENCE);
+     interval=setInterval(function(){
+         if(exper.getReady())
+         onPlayerReady();
+         }, 30);
+
+        
+}());
