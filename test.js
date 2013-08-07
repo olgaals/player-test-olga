960526697845dfd94ae7de0fc35a73174412e325
Olga <osidorova@gmail.com>
2013-08-07 16:48:48 +0300

    Update test.js

--- test.js
+++ test.js
@@ -6,8 +6,16 @@
     
      console.log("ready test share");
      clearInterval(interval);
+      window.open(
+      'https://www.facebook.com/sharer/sharer.php?u='+encodeURIComponent(location.href), 
+      'facebook-share-dialog', 
+      'width=626,height=436'); 
+    return false;
     
 }
+
+
+ 
 
 player = brightcove.api.getExperience();
     videoPl = player.getModule(brightcove.api.modules.APIModules.VIDEO_PLAYER);
